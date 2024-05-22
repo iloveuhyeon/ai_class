dcb6fcc5c37c18746bb379ba64b57648ce8e4317
parkyuhyeon <129172396+iloveuhyeon@users.noreply.github.com>
2024-05-22 15:38:19 +0900

    Update dataload.py

--- dataload.py
+++ dataload.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 st.title('Uber pickups in NYC')
 
@@ -29,3 +30,9 @@ data_load_state.text('Loading data...done!')
 # 부제목 만들기
 st.subheader('Raw data')
 st.write(data)
+
+# 시각화 코드 추가
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+
+st.bar_chart(hist_values)
