00e04cc363636a2e6ab4f140ca134b8764ca2a27
parkyuhyeon <129172396+iloveuhyeon@users.noreply.github.com>
2024-05-22 15:24:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.text("hello world")
 
-number = st.number_input("이름을 입력하세요!")
-st.write(f'현재 숫자는 {number}입니다.')
+text = st.text_input("이름을 입력하세요!")
+st.write("반갑습니다! " + text + "님!")
